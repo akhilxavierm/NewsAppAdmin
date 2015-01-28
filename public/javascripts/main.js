ba3c311dcd9dead3cbb8eee487bd7374b77c0ad9
akhilxavierm <akhilxavierm@gmail.com>
2015-01-28 16:27:14 +0530

    changesInMainJsforGrunt

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,32 +1,34 @@
 //////mainjs
 requirejs.config({
-                 baseUrl: 'javascripts/lib',
-                 paths: {
-                     collections : '../collections',
-                     models : '../models',
-                     views : '../views',
-                     util:'../util',
-                     templatePath: '../../templates'
-                 },
-                 shim: {
-                    'backbone': {
-                    deps: ['underscore', 'jquery'],
-                    exports: 'Backbone'
-                },
+    baseUrl: 'javascripts',
+    paths: {
 
-                 
-                 'underscore': {
-                 exports: '_'
-                 }
-                 }
+        templatePath: '../templates',
+        jquery:'lib/jquery',
+        backbone:'lib/backbone',
+        underscore:'lib/underscore',
+        handlebars:'lib/handlebars',
+        text:'lib/text'
+    },
+    shim: {
+        'backbone': {
+            deps: ['underscore', 'jquery'],
+            exports: 'Backbone'
+        },
+
+
+        'underscore': {
+            exports: '_'
+        }
+    }
 });
 
 
-require(['jquery', 'backbone','../router/router'], function ($, Backbone,appRouter ) {
-                                    console.log("inside way to router");
-    
-        
-                                    var router = new appRouter();
-                                    Backbone.history.start();
-             });
+require(['jquery', 'backbone','router/router'], function ($, Backbone,appRouter ) {
+    console.log("inside way to router");
+
+
+    var router = new appRouter();
+    Backbone.history.start();
+});
 
